cddc2dd21a83086712da5b22ce4f9b537cfc43cc
Muhammad Yuris Azmi <muhammad.yuris.azmi@gmail.com>
2014-03-29 08:52:18 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -237,7 +237,7 @@ echo "----------"  | tee -a log-install.txt
 echo "Webmin   : https://$MYIP:10000/"  | tee -a log-install.txt
 echo "vnstat   : http://$MYIP:81/vnstat/"  | tee -a log-install.txt
 echo "MRTG     : http://$MYIP:81/mrtg/"  | tee -a log-install.txt
-echo "Timezone : Asia/Makassar"  | tee -a log-install.txt
+echo "Timezone : Asia/Jakarta"  | tee -a log-install.txt
 echo "Fail2Ban : [on]"  | tee -a log-install.txt
 echo "IPv6     : [off]"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
